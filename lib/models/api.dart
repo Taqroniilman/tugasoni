045c838c874a6e0de4b1b34e43c0b4c7596b23d9
Taqroniilman <oniilman123@gmail.com>
2025-02-26 13:49:57 +0700

    Add files via upload

---
+++ lib/models/api.dart
@@ -0,0 +1,7 @@
+class BaseUrl {
+  static String tambah = "http://localhost/dataino/create.php";
+  static String lihat = "http://localhost/dataino/detail.php";
+  static String edit = "http://localhost/dataino/update.php";
+  static String hapus = "http://localhost/dataino/delete.php";
+  static String data = "http://localhost/dataino/list.php";
+}
